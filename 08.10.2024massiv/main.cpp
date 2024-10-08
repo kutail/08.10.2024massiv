5388b9eeaa0662fc410c28552ab01e5dd7553a6b
kutail <kutep0v.vlad.m@gmail.com>
2024-10-08 15:02:49 +0500

    Initial Commit

---
+++ 08.10.2024massiv/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  08.10.2024massiv
+//
+//  Created by Владислав on 08.10.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
