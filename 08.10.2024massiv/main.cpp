de37fcbc8a6c26a6ebcadc5c26fbad172515ecb7
kutail <kutep0v.vlad.m@gmail.com>
2024-10-08 15:57:37 +0500

    zadanie

--- 08.10.2024massiv/main.cpp
+++ 08.10.2024massiv/main.cpp
@@ -1,14 +1,53 @@
-//
-//  main.cpp
-//  08.10.2024massiv
-//
-//  Created by Владислав on 08.10.2024.
-//
+/*
+ Написать функцию, которая принимает массив и определяет минимальный и максимальный элемент массива
+ Показывает значение и номер элемента. Массив на 10 ячеек и заполняется рандомом
+ */
+
 
 #include <iostream>
+#include <cstdlib>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int Min_chislo(const int size, int arr[], int minchislo)
+{
+    for (int j = 0; j < size; j++)
+    {
+        if (arr[j]<minchislo)
+        {
+            minchislo = arr[j];
+        }
+    }
+    return minchislo;
+}
+
+int Max_chislo(const int size, int arr[], int maxchislo)
+{
+    for (int j = 0; j < size; j++)
+    {
+        if (arr[j]>maxchislo)
+        {
+            maxchislo = arr[j];
+        }
+    }
+    return maxchislo;
+}
+
+int main()
+{
+    srand(time(NULL));
+    
+    const int size = 10;
+    int maxchislo = 9;
+    int minchislo = 1;
+    int arr[size];
+    for (int j = 0; j < size; j++)
+    {
+        arr[j] = rand() % maxchislo + minchislo;
+        cout << arr[j] << "\t";
+    }
+    cout << "\nМаксимально число: " << Max_chislo(size, arr, maxchislo) << endl << "Минимальное число: " << Min_chislo(size, arr, minchislo) << endl;
+    
+    
     return 0;
 }
